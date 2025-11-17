dfe8651389c0f37e4555695783cbec8cf6f38980
Robert Wagner <rw@20sec.net>
2025-11-17 22:44:06 +0100

    csv

--- install-dev.sh
+++ install-dev.sh
@@ -12,3 +12,14 @@ ln -s ../pickel studio
 # mlr --icsv --ifs ';' --ojsonl put '$_id="b.".$id;$_type="booking";$house="usedom";$address2="".$address2;' ../lodge/usedom.csv > bookings.jsonl
 
 # time curl 'http://localhost:9902/data/mutate/push/booking'  -H 'Content-Type: application/json' --data-binary @bookings.jsonl
+
+#ssh remote_server \
+    "mysql \
+        --silent \
+        --raw \
+        --host "" --port 3306 \
+        --user "" --password="" \
+        table \
+        -e \"SELECT JSON_OBJECT('key', value) FROM table\" |
+    jq --slurp --ascii-output ." \
+    > dump.json
