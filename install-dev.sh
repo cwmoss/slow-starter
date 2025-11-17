70a71d2b9e4afd2bf02607da4a564f7165c9d453
Robert Wagner <rw@20sec.net>
2025-11-17 22:22:54 +0100

    csv

--- install-dev.sh
+++ install-dev.sh
@@ -8,6 +8,7 @@ ln -s ../pickel studio
 
 # docker compose exec web slowhand/backend/bin/slowhand setup
 
+# sed s/\\r//g < usedom.csv > u.csv
 # mlr --icsv --ifs ';' --ojsonl put '$_id="b.".$id;$_type="booking";$house="usedom";$address2="".$address2;' ../lodge/usedom.csv > bookings.jsonl
 
 # time curl 'http://localhost:9902/data/mutate/push/booking'  -H 'Content-Type: application/json' --data-binary @bookings.jsonl
